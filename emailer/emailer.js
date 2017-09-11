bd9a4db3d55d838fed12c7f9b2fcd1e681f3dff3
Max Berger <maxamillianberger@gmail.com>
2017-09-11 18:58:03 -0500

    Incorporate SendGrid email service

--- emailer/emailer.js
+++ emailer/emailer.js
@@ -1,3 +1,15 @@
-var requestHandler = function() {};
+var sendGrid = require('./sendGrid');
+
+var requestHandler = function(request, response) {
+  var message = {
+    to: 'maxamillian_berger@yahoo.com',
+    from: 'Max Berger <maxamillianberger@gmail.com>',
+    subject: 'Sending email through SendGrid',
+    text: 'Great, it worked!',
+    html: '<strong>Great, it worked!</strong>',
+  };
+
+  sendGrid.sendEmail(message);
+};
 
 module.exports.requestHandler = requestHandler;
